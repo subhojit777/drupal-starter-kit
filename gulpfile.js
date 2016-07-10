c264f3b2ec459f0696e14dc7d8f9021294d88406
Ashish Thakur <dash2ashish@gmail.com>
2016-07-10 12:41:41 +0530

    Added css lint configuration

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var eslint = require('gulp-eslint');
 var es = require('event-stream');
+var csslint = require('gulp-csslint');
 
 gulp.task('jslint', function() {
   var modules = gulp.src('docroot/sites/all/modules/custom/**/*.js');
@@ -10,6 +11,16 @@ gulp.task('jslint', function() {
     .pipe(eslint())
     .pipe(eslint.format())
     .pipe(eslint.failAfterError());
+});
+
+gulp.task('csslint', function() {
+  var modules = gulp.src('docroot/sites/all/modules/custom/**/*.css');
+  var themes = gulp.src('docroot/sites/all/themes/**/*.css');
+
+  return es.merge(modules, themes)
+    .pipe(csslint())
+    .pipe(csslint.reporter())
+    .pipe(csslint.reporter('fail'));
 });
 
 gulp.task('default', [
