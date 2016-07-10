fe3da384ceabc92487036d8d61e9f28bca9e19c5
Ashish Thakur <dash2ashish@gmail.com>
2016-07-10 11:08:10 +0530

    Changed error format

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,16 @@ var gulp = require('gulp');
 var eslint = require('gulp-eslint');
 var es = require('event-stream');
 
-gulp.task('default', function() {
+gulp.task('jslint', function() {
   var modules = gulp.src('docroot/sites/all/modules/custom/**/*.js');
   var themes = gulp.src('docroot/sites/all/themes/**/*.js');
 
   return es.merge(modules, themes)
     .pipe(eslint())
     .pipe(eslint.format())
-    .on('error', function(err) { throw err; });;
+    .pipe(eslint.failAfterError());
 });
+
+gulp.task('default', [
+  'jslint'
+]);
