d9f902068461c2998d82661a3ad61d91e6b3fbc6
Ashish Thakur <dash2ashish@gmail.com>
2016-07-17 15:25:56 +0530

    Bug fixes and refactoring

--- gulpfile.js
+++ gulpfile.js
@@ -60,21 +60,15 @@ gulp.task('php', function () {
   sourcePatterns = sourcePatterns.concat(excludePatterns);
 
   // Run phpcs.
-  gulp.src(sourcePatterns)
-   .pipe(tap(function (file) {
-    var report = execSync('./vendor/bin/phpcs --standard="./.phpcsrc.xml" ' + file.path);
-    if (report.stdout.length > 0) {
-      // Log report, and remove silly Code Sniffer 2.0 ad.
-      console.log(report.stdout.split('UPGRADE TO PHP_CODESNIFFER 2.0 TO FIX ERRORS AUTOMATICALLY')[0]);
-    }
-  }));
-
-  // Llint php source files.
-  phplint(sourcePatterns, {limit: 50}, function (err, stdout, stderr) {
-    if (err) {
-       console.log(err);
-     }
-  });
+  return gulp.src(sourcePatterns)
+    .pipe(tap(function (file) {
+      var report = execSync('./vendor/bin/phpcs --standard="./.phpcsrc.xml" ' + file.path);
+      if (report.stdout.length > 0) {
+        // Log report, and remove silly Code Sniffer 2.0 ad.
+        console.log(report.stdout.split('UPGRADE TO PHP_CODESNIFFER 2.0 TO FIX ERRORS AUTOMATICALLY')[0]);
+      }
+    }))
+    .pipe(phplint.reporter('fail'));
 });
 
 gulp.task('default', [
